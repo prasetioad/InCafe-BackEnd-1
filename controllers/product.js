f10d35cd1cd49c076908504b1fa82c8b5c445543
prasetioad <waghupd@gmail.com>
2021-04-07 16:58:19 +0700

    Menambahkan Product endpoint,router, controller, dan models, masih belum menggunakan MULTER

--- controllers/product.js
+++ controllers/product.js
@@ -3,11 +3,9 @@ const formatResult = require("../helpers/formatResult");
 const Product = db.product;
 
 exports.create = (req, res) => {
-    const{ name, price, description, size, startHour, endHour, stock, deliveryMethod} = req.body
-    const data = { name, price, description, size, startHour, endHour, stock, deliveryMethod}
-    Product.create(data)
-      .then(() => {
-        formatResult(res, 201, true, "Product Created!", {data});
+    Product.create(req.body)
+      .then((result) => {
+        formatResult(res, 201, true, "Product Created!", result.dataValues);
       })
       .catch((err) => {
         formatResult(res, 500, false, err, null);
@@ -17,6 +15,14 @@ exports.create = (req, res) => {
 exports.getData = (req, res) => {
   Product.findAll()
     .then((result) => {
+        if(result.length > 0){
+            const dataResult = result.map((item)=>{
+                item.size = JSON.parse(item.size)
+                item.deliveryMethod = JSON.parse(item.deliveryMethod)
+                return item
+            })
+            console.log(dataResult);
+        }
       formatResult(res, 200, true, "Success Get Product!", result);
     })
     .catch((err) => {
@@ -57,10 +63,8 @@ exports.deleteData = (req, res) =>{
 
 exports.updateData = (req, res) => {
     const productId = req.params.id
-    const { name, price, description, size, startHour, endHour, stock, deliveryMethod } = req.body
-    const data = { name, price, description, size, startHour, endHour, stock, deliveryMethod }
     console.log('ini adalah params = ', productId,);
-    Product.update(data, { where: { id: productId }})
+    Product.update(req.body, { where: { id: productId }})
         .then((result) => {
             formatResult(res, 201, true, "Update Success", result);
         })
